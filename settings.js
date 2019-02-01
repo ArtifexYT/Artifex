7430555bd3272dde4439cbbc718f71a2ec263ec0
Damian <46952101+ArtifexYT@users.noreply.github.com>
2019-02-01 10:47:53 -0500

    Added configuration file.

---
+++ settings.js
@@ -0,0 +1,5 @@
+module.exports = {
+    token: process.env.DISCORD,
+    modRoles: ["Alaina", "The Kid Upstairs", "Deputy Moderator", "Moderator"],
+    adminRoles: ["Alaina", "The Kid Upstairs", "Deputy Moderator"]
+};
